6b58cc9de045dc04db46c0df33829704004602fa
nemet3D <nemet3D@gmail.com>
2019-12-26 07:40:39 +0100

    Exercises added

---
+++ ch2_3_angle.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+point_x = 1.0
+point_y = -1.0
+
+theta = np.arctan(point_y/point_x)
+
+print(theta)
---
+++ ch2_4_unit_vector.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+theta =  (3*np.pi/2) * (180/np.pi)
+vector = [round(np.cos(theta), 2), round(np.sin(theta), 2)]
+
+print(vector)
